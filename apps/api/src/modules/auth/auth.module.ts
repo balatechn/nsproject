1c417d4fa07d4c73f1daa44fd7b37b09d2f5453f
NsProject <admin@nsproject.com>
2026-05-16 14:37:33 +0530

    fix: move prisma to deps (not devDeps), fix JwtService DI in NotificationsModule

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -23,6 +23,6 @@ import { UsersModule } from '../users/users.module';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, LocalStrategy, GoogleStrategy],
-  exports: [AuthService],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
